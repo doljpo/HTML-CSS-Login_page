a67e21230689d6c79653ea7c6373c3f90eb4f2b9
doljpo <dol.jpo@gmail.com>
2020-09-20 23:29:47 -0300

    Moving script to a specific js file

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+var loginPosition = document.getElementById("login-form");
+var registerPosition = document.getElementById("register-form");
+var selectedButtonPosition = document.getElementById("selected-option");
+
+function showLoginForm() {
+    loginPosition.style.left = "0%";
+    registerPosition.style.left = "100%";
+    selectedButtonPosition.style.left = "0%";
+}
+
+function showRegisterForm() {
+    loginPosition.style.left = "-100%";
+    registerPosition.style.left = "0%";
+    selectedButtonPosition.style.left = "50%";
+}
